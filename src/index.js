37c03b03509145b6b531ccc4e9df9f09563c81aa
jshou <joshuahou@gmail.com>
2016-01-30 13:03:14 -0800

    hero

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public/views'))
 app.use('/js', express.static(__dirname + '/public/js'))
 app.use('/css', express.static(__dirname + '/public/css'))
+app.use('/img', express.static(__dirname + '/public/img'))
 
 app.listen(PORT, function(){
   console.log('Caravan server running at Port', PORT)
