632079a168adcb779d047a29db42389f8e715257
Wale O <wale@screenverve.com>
2016-01-29 23:37:37 -0800

    figure out stupid directory error

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  PORT = 3000,
+  app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static(__dirname + '/public/views'))
+
+app.listen(PORT, function(){
+  console.log('Caravan server running at Port', PORT)
+})
