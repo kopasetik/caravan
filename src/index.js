6c115686b04f6d7b6f21aebad62bca6cada918f5
Wale O <wale@screenverve.com>
2016-01-30 12:36:28 -0800

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const
   express = require('express'),
+  express = require('express-session'),
   bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
   path = require('path'),
   PORT = 3000,
   app = express()
+
+mongoose.connect('mongodb://localhost/expedia')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -11,6 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public/views'))
 app.use('/js', express.static(__dirname + '/public/js'))
 app.use('/css', express.static(__dirname + '/public/css'))
+
+app.use('/api/users', require())
 
 app.listen(PORT, function(){
   console.log('Caravan server running at Port', PORT)
